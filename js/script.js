b0bdfbfae18a9fea93631ac1db1c9d7a74babbb3
Adam Johnosn <john14465@hotmail.com>
2017-07-01 20:54:48 -0500

    changed content, add parallex, cleaned up code

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ $(document).ready(function(){
     var imgs = ["img/45c0a4a2937f3a5540c5550e2330afdd.jpg", "img/autumn_bench-HD.jpg", "img/car-wallpapers13.jpg", "img/fenyr-supersport-938.jpg", "img/hd-wallpaper-48.jpg", "img/ship-wallpapers-16.jpg"];
     var img = $("#landing");
 
-    // img.css("background-image", "url("+imgs[count]+")");
-
     setInterval(function(){
         img.fadeOut(600, function(){
             img.css("background-image", "url("+imgs[count++]+")");
@@ -13,6 +11,6 @@ $(document).ready(function(){
         if (count == imgs.length){
             count = 0;
         }
-    }, 2500);
+    }, 3000);
 }); 
 
